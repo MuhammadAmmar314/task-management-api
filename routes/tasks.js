b97a7666932882f11de350e19dbb35d9a1159da7
Muhammad Ammar Abdurrahim <muhammadammar314@gmail.com>
2023-06-20 19:48:52 +0700

    add delete task

--- routes/tasks.js
+++ routes/tasks.js
@@ -140,4 +140,25 @@ router.patch('/tasks/(:id)', [
     })
 })
 
+/**
+ * DELETE TASK
+ */
+router.delete('/tasks/(:id)', function(req,res){
+    let id = req.params.id;
+
+    connection.query(`DELETE FROM tasks WHERE id=${id}`, function(err,rows){
+        if(err){
+            return res.status(500).json({
+                status : false,
+                message : 'Internal Server Error'
+            })
+        } else {
+            return res.status(200).json({
+                status : true,
+                message : 'Delete Data Successfully'
+            })
+        }
+    })
+})
+
 module.exports = router;
